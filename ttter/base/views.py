3bbbf65efc7101b7466f838825087f6c16883cf7
YukiY727 <y.yamada@4nonome.com>
2022-06-20 10:56:12 +0900

    base viewsのいらないコード削除

--- ttter/base/views.py
+++ ttter/base/views.py
@@ -3,8 +3,3 @@ from django.views.generic import TemplateView
 
 class TopView(TemplateView):
     template_name = 'base/top.html'
-
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        ctx['title'] = 'TTTer'
-        return ctx
