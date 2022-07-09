96ba691fb5c146f44791cddd66f66940cd27d377
YukiY727 <y.yamada@4nonome.com>
2022-07-09 11:40:21 +0900

    detailの途中まで

--- ttter/ttter/urls.py
+++ ttter/ttter/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls'), name='base'),
     path('accounts/', include('accounts.urls')),
+    path('tweet/', include('tweet.urls')),
 ]
--- ttter/tweet/views.py
+++ ttter/tweet/views.py
@@ -1,7 +1,19 @@
-from forms import TweetForm
+from django.urls import reverse_lazy
+
+from .forms import TweetForm
+from .models import Tweet
 
 
-class DiaryCreateView(CreateView):
-    template_name = 'diary_create.html'
+class TweetCreateView(CreateView):
+    template_name = 'tweet:tweet_create.html'
     form_class = TweetForm
-    success_url = reverse_lazy('diary:diary_create_complete')
+    success_url = reverse_lazy('base:top')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class TweetListView(ListView):
+    template_name = 'tweet:tweet_list.html'
+    model = Tweet
