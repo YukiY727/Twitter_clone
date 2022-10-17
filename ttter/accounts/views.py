bc634c705e5573a02aec6f9df2ffff74e3c56de0
YukiY727 <y.yamada@4nonome.com>
2022-10-17 17:47:07 +0900

    prefetch_related使ってみたが、うまくいかない

--- ttter/accounts/views.py
+++ ttter/accounts/views.py
@@ -79,7 +79,7 @@ class FollowerListView(LoginRequiredMixin, TemplateView):
         ctx = super().get_context_data(**kwargs)
         self.user = get_object_or_404(User, username=self.kwargs["username"])
 
-        ctx["followers"] = [follower for follower in self.user.followees.all()]
+        ctx["followers"] = [follower for follower in self.user.all().prefetch_related("followees")]
 
         return ctx
 
@@ -90,7 +90,7 @@ class FollowingListView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         self.user = get_object_or_404(User, username=self.kwargs["username"])
-        ctx["followings"] = [follower for follower in self.user.followers.all()]
+        ctx["followings"] = [follower for follower in self.user.all().prefetch_related("followers")]
         return ctx
 
 
