4993b721f06ed889d91ec6e839fc819c7129b547
YukiY727 <y.yamada@4nonome.com>
2022-10-28 12:22:58 +0900

    follow, unfollow時に正常ではない操作を行った際、render先を直すことで、正常な画面が表示されるようにした

--- ttter/accounts/views.py
+++ ttter/accounts/views.py
@@ -102,12 +102,14 @@ class FollowView(LoginRequiredMixin, TemplateView):
         followee = get_object_or_404(User, id=self.request.user.id)
         if followee == follower:
             messages.warning(self.request, "自分自身はフォローできません。")
+            return render(self.request, "accounts/follow.html", {"follower": follower})
         else:
             _, created = FriendShip.objects.get_or_create(
                 followee=followee, follower=follower
             )
             if not created:
                 messages.warning(self.request, f"{follower.username}さんはすでにフォローしています。")
+                return render(self.request, "accounts/follow.html", {"follower": follower})
         return redirect("tweet:home")
 
 
@@ -124,8 +126,10 @@ class UnFollowView(LoginRequiredMixin, TemplateView):
         followee = get_object_or_404(User, id=self.request.user.id)
         if followee == follower:
             messages.warning(self.request, "自分自身のフォロー解除はできません。")
+            return render(self.request, "accounts/unfollow.html", {"follower": follower})
         if FriendShip.objects.filter(followee=followee, follower=follower).exists():
             FriendShip.objects.filter(followee=followee, follower=follower).delete()
         else:
             messages.warning(self.request, f"{follower.username}さんはフォローしていません。")
+            return render(self.request, "accounts/unfollow.html", {"follower": follower})
         return redirect("tweet:home")
